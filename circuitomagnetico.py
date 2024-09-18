f5668b33e09c9170a6448490e2906b9ac0b92ba0
Santiago Herra <tato.herra@gmail.com>
2024-09-18 16:50:46 -0600

    Terminada solucion del circuito, falta debug

--- circuitomagnetico.py
+++ circuitomagnetico.py
@@ -16,7 +16,8 @@ def dispersion_flujo_real(coe_dispersion, flujo_entrehierro):
     
 #Aqui es la funcion de la solucion principal del circuito
 def solucion_circuitomagentico(valores_magnitudes_electricas, valores_dimensiones):
-    bobina1 = False
+    bobina1_bandera = False
+    flujo1_bandera = False
 
     #asignar valores a variables de magnitudes electricas
     N_1, N_2, I_1, I_2, flujo_E = valores_magnitudes_electricas
@@ -27,7 +28,7 @@ def solucion_circuitomagentico(valores_magnitudes_electricas, valores_dimensione
     #Se seleccionado cual bobina es la que se le dio el valor de corriente osea i1 o i2
     if I_1:
         fmm_bobina1 = N_1 * I_1
-        bobina1 = True
+        bobina1_bandera = True
     else:
         fmm_bobina2 = N_2 * I_2
 
@@ -49,7 +50,51 @@ def solucion_circuitomagentico(valores_magnitudes_electricas, valores_dimensione
     #Obtengo la fmm de la barra total
     fmm_barra_central_total = fmm_reluctancia_barra + fmm_entrehierro
 
+    #Obtengo la fmm de la reluctancia de la malla donde me dieron la corriente 
+    #ademas obtengo el H y con eso B del flujo y por ultimo el flujo.
+
+    if bobina1_bandera:
+        fmm_reluctancia_malla1 = fmm_barra_central_total - fmm_bobina1
+        H_flujo_1 = fmm_reluctancia_malla1 / 2 * L1 + A 
+        B_flujo1 = funcion_H_B(H_flujo_1)
+        flujo1 = B_flujo1 / SL
+        flujo2  = flujo1 + flujo_E
+        flujo1_bandera = True
+
+    else:
+        fmm_reluctancia_malla2 = fmm_barra_central_total - fmm_bobina2
+        H_flujo_2 = fmm_reluctancia_malla2 / 2 * L2 + A
+        B_flujo2 = funcion_H_B(H_flujo_1)
+        flujo2 = B_flujo2 / SL
+        flujo1  = flujo2 + flujo_E
+
+    #Obtengo la corriente que me falta.
+    #Primero obtengo la fuerza de la reluctancia en la malla que me hace falta.
+    #Despues con ltk despues la corriente que hace falta.
+
+    if not flujo1_bandera:
+        B_flujo1 = flujo1 / SL
+        H_flujo_1 = funcion_H_B(B_flujo1)
+        fmm_reluctancia_malla1 = H_flujo_1 * (2*L1 + A)
+        I_1 = fmm_barra_central_total + fmm_reluctancia_malla1 / N_1
     
+    else:
+        B_flujo2 = flujo2 / SL
+        H_flujo_2 = funcion_H_B(B_flujo2)
+        fmm_reluctancia_malla2 = H_flujo_2 * (2*L2 + A)
+        I_2 = fmm_barra_central_total + fmm_reluctancia_malla1 / N_1
+
+    return I_1, I_2, flujo1, flujo2
+
+
+
+    
+    
+    
+    
+
+    
+     
 
 
 
